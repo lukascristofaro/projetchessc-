1037a665dca848494a042ff886abc7be9c5d8e88
Doctorwho07 <alexis.rouches@icloud.com>
2024-01-25 09:11:37 +0100

    first push

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,13 +12,59 @@ using System.Windows.Shapes;
 namespace chess
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Label label = (Label)this.FindName("placeholderLabel");
+            if (label != null)
+            {
+                label.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            Label label = (Label)this.FindName("placeholderLabel");
+
+            if (textBox != null && label != null)
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    label.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_JoinRoom_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
